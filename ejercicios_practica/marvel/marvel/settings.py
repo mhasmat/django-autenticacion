0f209a5fc611c509419848908e013c8fe0baa56c
Marto <53458858+mhasmat@users.noreply.github.com>
2024-04-29 23:45:20 -0300

    upload tarea 2

--- ejercicios_practica/marvel/marvel/settings.py
+++ ejercicios_practica/marvel/marvel/settings.py
@@ -43,7 +43,7 @@ BASE_APPS = [
 ]
 
 # Acá van las apps de 3ros que necesitamos agregar para que Django las encuentre.
-THIRD_APPS = ['rest_framework']
+THIRD_APPS = ['rest_framework', 'rest_framework.authtoken', 'drf_yasg']
 
 # Acá van las apps que creamos nosotros.
 LOCAL_APPS = ['e_commerce']
@@ -61,15 +61,18 @@ INSTALLED_APPS = BASE_APPS + THIRD_APPS + LOCAL_APPS
 #     'e_commerce',
 #     # Third party apps: acá vamos agregando las aplicaciones de terceros, extensiones de Django.
 #     'rest_framework',
+#     'rest_framework.authtoken',
+#     'drf_yasg',
 # ]
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
 }
-
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -102,7 +105,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'marvel.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -179,7 +181,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 # NOTE: Para debug
 
 # Color en los prints:
@@ -191,3 +192,37 @@ VERDE = "\033[;32m"
 
 # NOTE: Para manejo de sesión.
 LOGIN_URL = '/admin/login'
+
+# API DOCS Settings:
+LOGOUT_URL = '/admin/logout'
+
+# Acá van todas las configuraciones para la UI de Swagger.
+SWAGGER_SETTINGS = {
+    'DEFAULT_MODEL_RENDERING': "example",
+    # Seteo los tipos de Authenticaciones que puedo utilizar en
+    # Swagger.
+    # https://drf-yasg.readthedocs.io/en/stable/settings.html#security-definitions-settings
+    'SECURITY_DEFINITIONS': {
+        # HTTP Basic Authentication:
+        'basic': {
+            'description': "Basic Auth",
+            'type': 'basic',
+            'in': 'header'
+        },
+        # Token Authentication:
+        'DRF Token': {
+            'description': '**Ejemplo: Token ea0dfcbbdff1a55ae26a67cd71bcc6adffb1f200**',
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+         }
+    },
+    "USE_SESSION_AUTH": True,
+    'LOGIN_URL': LOGIN_URL,
+    'LOGOUT_URL': LOGOUT_URL
+}
+
+# Acá van todas las configuraciones para la UI de Redoc.
+REDOC_SETTINGS = {
+   'LAZY_RENDERING': False,
+}
